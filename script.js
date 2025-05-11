a3eeb2d9aac394b4eb5e21ed813120700b857034
GuiJK <junquekarabe18@gmail.com>
2025-05-11 15:32:10 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function mostrarFoto() {
+  const foto = document.getElementById("love-photo");
+  foto.style.display = "block";
+}
+
+function tocarMusica() {
+  const link = "https://www.youtube.com/watch?v=Mkf1IHBElMo"; // Substitua com o link real
+  window.open(link, "_blank");
+}
